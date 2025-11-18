f7af3d2392ac5a771c5f19b991722776960d0d1c
Samantha Perry <samnperry@arizona.edu>
2025-11-18 12:45:46 -0700

    add: login and signup and user auth

--- heart-rate-monitor/server/index.js
+++ heart-rate-monitor/server/index.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+require("./db");
+
+var authRoutes = require("./routes/authRoutes");
+var deviceRoutes = require("./routes/deviceRoutes");
 
 var app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use("/api/auth", authRoutes);
+app.use("/api/device", deviceRoutes);
+
+app.listen(5001, function() {
+    console.log("Server running on port 5001")
 });
