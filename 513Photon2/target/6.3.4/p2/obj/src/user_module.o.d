599fc6bd2acbc9022b50e678eb655cda2b85a941
Samantha Perry <samnperry@arizona.edu>
2025-11-20 22:15:25 -0700

    Reorganize project file structure

--- 513Photon2/target/6.3.4/p2/obj/src/user_module.o.d
+++
@@ -1,7 +0,0 @@
-/Users/sevengilbert/Desktop/ECE513FinalProject/513Photon2/target/6.3.4/p2//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
-../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
-../../../hal/shared/platforms.h:
